a34a5ccff8c15ab7d2b678040b9aa1aea4ab8fa0
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-22 16:05:44 +0900

    feat : 에러바운더리/서스펜스/라우터 적용

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import styled from '@emotion/styled';
+import { ErrorBoundary } from 'react-error-boundary';
 import { ThemeProvider } from '@emotion/react';
+
 import theme from './static/style/theme';
+import Loader from '@/components/Loader';
+import NotFound from './pages/NotFound';
+
+import { Router, Switch, Route, Link } from './core/Router';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <PageContainer></PageContainer>
+      <ErrorBoundary fallbackRender={Loader}>
+        <Suspense fallback={Loader}>
+          <PageContainer>
+            <Router>
+              <Switch>
+                <Route path="/">
+                  <div>Root</div>
+                  <Link to="/">Go Root</Link>
+                  <Link to="/hi">Go Hi</Link>
+                  <Link to="/bye">Go Bye</Link>
+                </Route>
+                <Route path="/hi">
+                  <div>hi</div>
+                  <Link to="/">Go Root</Link>
+                  <Link to="/hi">Go Hi</Link>
+                  <Link to="/bye">Go Bye</Link>
+                </Route>
+                <Route path="/bye">
+                  <div>Bye</div>
+                  <Link to="/">Go Root</Link>
+                  <Link to="/hi">Go Hi</Link>
+                  <Link to="/bye">Go Bye</Link>
+                </Route>
+                <Route path="/:notfound">
+                  <NotFound />
+                </Route>
+              </Switch>
+            </Router>
+          </PageContainer>
+        </Suspense>
+      </ErrorBoundary>
     </ThemeProvider>
   );
 };
