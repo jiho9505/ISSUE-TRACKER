9d5f63994faf1fbe7b96741f5d47d869d839ea93
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-22 17:06:14 +0900

    feat: Adjust react-query in App.js

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -2,50 +2,58 @@ import React, { Suspense } from 'react';
 import styled from '@emotion/styled';
 import { ErrorBoundary } from 'react-error-boundary';
 import { ThemeProvider } from '@emotion/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import Loader from './components/Loader';
+import NotFound from './pages/NotFound';
+import Error from './components/Error';
 
 import theme from './static/style/theme';
-import Loader from '@/components/Loader';
-import NotFound from './pages/NotFound';
-
 import { Router, Switch, Route, Link } from './core/Router';
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <ErrorBoundary fallbackRender={Loader}>
-        <Suspense fallback={Loader}>
-          <PageContainer>
-            <Router>
-              <Switch>
-                <Route path="/">
-                  <div>Root</div>
-                  <Link to="/">Go Root</Link>
-                  <Link to="/hi">Go Hi</Link>
-                  <Link to="/bye">Go Bye</Link>
-                </Route>
-                <Route path="/hi">
-                  <div>hi</div>
-                  <Link to="/">Go Root</Link>
-                  <Link to="/hi">Go Hi</Link>
-                  <Link to="/bye">Go Bye</Link>
-                </Route>
-                <Route path="/bye">
-                  <div>Bye</div>
-                  <Link to="/">Go Root</Link>
-                  <Link to="/hi">Go Hi</Link>
-                  <Link to="/bye">Go Bye</Link>
-                </Route>
-                <Route path="/:notfound">
-                  <NotFound />
-                </Route>
-              </Switch>
-            </Router>
-          </PageContainer>
-        </Suspense>
-      </ErrorBoundary>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <ErrorBoundary FallbackComponent={Error}>
+          <Suspense fallback={Loader}>
+            <PageContainer>
+              <Router>{RouterContent}</Router>
+            </PageContainer>
+          </Suspense>
+        </ErrorBoundary>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
+
+const RouterContent = (
+  <Switch>
+    <Route path="/">
+      <div>Root</div>
+      <Link to="/">Go Root</Link>
+      <Link to="/hi">Go Hi</Link>
+      <Link to="/bye">Go Bye</Link>
+    </Route>
+    <Route path="/hi">
+      <div>hi</div>
+      <Link to="/">Go Root</Link>
+      <Link to="/hi">Go Hi</Link>
+      <Link to="/bye">Go Bye</Link>
+    </Route>
+    <Route path="/bye">
+      <div>Bye</div>
+      <Link to="/">Go Root</Link>
+      <Link to="/hi">Go Hi</Link>
+      <Link to="/bye">Go Bye</Link>
+    </Route>
+    <Route path="/:notfound">
+      <NotFound />
+    </Route>
+  </Switch>
+);
 
 const PageContainer = styled.div`
   width: 100vw;
