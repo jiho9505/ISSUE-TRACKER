ffaa580ac7b993ab5550c5cd98e2d4782fe33a82
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-22 16:35:19 +0900

    feat : 에러바운더리 전용 컴포넌트 추가

--- client/src/components/Error/index.jsx
+++ client/src/components/Error/index.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from '@emotion/styled';
 import { ERROR_IMG } from '@/static/constants/image-path';
+import { useTheme } from '@emotion/react';
 
-const Error = () => {
+const Error = ({ error, resetErrorBoundary }) => {
+  const theme = useTheme();
   return (
-    <ErrorContainer>
-      <span>Error occured</span>
+    <ErrorContainer bgColor={theme.colors.background}>
+      <span>{error.message}</span>
       <img className="error" src={ERROR_IMG} alt="에러" />
+      <Button onClick={resetErrorBoundary} bgColor={theme.colors.blue}>
+        Try again
+      </Button>
     </ErrorContainer>
   );
 };
@@ -18,11 +23,26 @@ const ErrorContainer = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  width: 100%;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
+  background-color: ${(props) => props.bgColor};
 
+  span {
+    font-size: 1.2rem;
+  }
   .error {
     width: 20%;
     height: 30%;
   }
+`;
+
+const Button = styled.button`
+  border: none;
+  border-radius: 18px;
+  width: 200px;
+  height: 60px;
+  font-size: 1.3rem;
+  color: white;
+  background-color: ${(props) => props.bgColor};
+  cursor: pointer;
 `;
