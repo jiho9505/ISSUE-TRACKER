8ecc6f4ebdc5bdeb7666bfe83ef211ecdb762d17
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-23 16:22:50 +0900

    style : Add mixin style

---
+++ client/src/static/style/mixin.js
@@ -0,0 +1,12 @@
+import { css } from '@emotion/react';
+
+export const allCenterAlign = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const garoCenterAlign = css`
+  display: flex;
+  justify-content: center;
+`;
