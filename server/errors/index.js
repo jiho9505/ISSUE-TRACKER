563f6cb1bd5881902cd23c3f710ad0e7fc8b62e3
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-24 18:42:23 +0900

    feat : Add server error msg

---
+++ server/errors/index.js
@@ -0,0 +1,13 @@
+export const error = {
+  NOT_LOGIN_ERROR: '로그인이 필요한 서비스입니다.',
+  JWT_TOKEN_INVALID_ERROR: '더 이상 유효하지 않은 토큰입니다.',
+  GET_COMMENT_ERROR: '댓글 목록을 가져오는데 실패하였습니다.',
+  CREATE_COMMENT_ERROR: '댓글을 게시하는데 실패하였습니다.',
+  DELETE_COMMENT_ERROR: '댓글를 삭제하는데 실패하였습니다.',
+  COMMON_ERROR: '처리 과정 중 에러가 발생했습니다',
+  GET_BOARD_ERROR: '게시물을 가져오는데 에러가 발생했습니다',
+  CREATE_IMAGE_ERROR: '이미지를 추가하는데 에러가 발생했습니다',
+  CREATE_BOARD_ERROR: '게시글을 추가하는데 에러가 발생했습니다',
+  UPDATE_BOARD_ERROR: '게시글을 수정하는데 에러가 발생했습니다',
+  DELETE_BOARD_ERROR: '게시글을 삭제하는데 에러가 발생했습니다',
+};
