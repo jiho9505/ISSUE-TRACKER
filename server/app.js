1c364bf64c4b76548582f381286ac6b6b45ada31
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-24 18:42:11 +0900

    feat : DB 연결

--- server/app.js
+++ server/app.js
@@ -2,26 +2,34 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import path from 'path';
-// import ApiRouter from './routes/index.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import ApiRouter from './routes/index.js';
 
+dotenv.config();
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB is connected...');
+  })
+  .catch((err) => console.log(err));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 app.use(cookieParser());
-// app.use('/api', ApiRouter);
+app.use('/api', ApiRouter);
 
-app.use(express.static('./build'));
+app.use(express.static('server/build'));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve('./build', 'index.html'));
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server Listening on ${port}`);
 });
-
-export default app;
