4d4b65bc3e3970d4b949d3a66d080bba5c9e8fe2
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-25 12:15:26 +0900

    feat : Add User Routes
    - login (jwt)
    - logout
    - register

---
+++ server/routes/index.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+import UserRouter from './users.js';
+
+const ApiRouter = Router();
+
+export default ApiRouter;
---
+++ server/routes/users.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+import { UserController } from './../controllers/UserController.js';
+import { auth } from '../middleware/auth.js';
+
+const router = express.Router();
+
+router.get('/logout', auth.checkLogin, UserController.logout);
+router.post('/register', UserController.registerUser);
+router.post('/login', UserController.login);
+
+export default router;
