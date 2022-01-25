12adc040e5d954e8a3c66cc78a46057642b63bc6
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-25 12:13:28 +0900

    feat : Add getRandomColor fn

--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -3,3 +3,9 @@ export const delay = (time) => {
     setTimeout(resolve, time);
   });
 };
+
+export const getRandomColor = () => {
+  const hexCode = Math.floor(Math.random() * 16777215).toString(16);
+  const result = hexCode.padStart(6, '0');
+  return result;
+};
