90c121ee2315e314d87a376b1cfc9a92faf482ac
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-25 12:16:29 +0900

    feat  : Add server auth fn

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,12 +1,13 @@
-import { error } from '../errors';
+import { error } from '../errors/index.js';
+import jwt from 'jsonwebtoken';
 
 const checkLogin = (req, res, next) => {
   try {
     const token = req.cookies.w_auth;
     if (!token) throw new Error(error.NOT_LOGIN_ERROR);
-    jwt.verify(token, 'secret', (err) => {
+    jwt.verify(token, 'secret', (err, decode) => {
       if (err) throw new Error(error.JWT_TOKEN_INVALID_ERROR);
-      req.id = token;
+      req._id = decode._id;
       next();
     });
   } catch (e) {
