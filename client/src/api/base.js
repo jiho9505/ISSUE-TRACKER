d7563c28f61b61bdcbb1167f88d0a894600ecedf
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-27 17:15:14 +0900

    refactor: API 파일 중복로직 제거

--- client/src/api/base.js
+++ client/src/api/base.js
@@ -2,68 +2,30 @@ import axios from 'axios';
 
 const API_PREFIX = process.env.API_PREFIX;
 
+const request = async (url, method, body = {}) => {
+  try {
+    const API_ENDPOINT = API_PREFIX + url;
+    const { data } =
+      (method === 'get' && (await axios.get(API_ENDPOINT))) ||
+      (method === 'post' && (await axios.post(API_ENDPOINT, body))) ||
+      (method === 'put' && (await axios.put(API_ENDPOINT, body))) ||
+      (method === 'delete' && (await axios.delete(API_ENDPOINT)));
+
+    if (!data.success) throw new Error(data.message);
+    return {
+      isSuccess: true,
+      data,
+    };
+  } catch (e) {
+    return {
+      isSuccess: false,
+      message: e.message,
+    };
+  }
+};
 export const api = {
-  get: async (url) => {
-    try {
-      const { data } = await axios.get(`${API_PREFIX}${url}`);
-      if (!data.success) throw new Error(data.message);
-      return {
-        isSuccess: true,
-        data,
-      };
-    } catch (e) {
-      return {
-        isSuccess: false,
-        message: e.message,
-      };
-    }
-  },
-
-  post: async (url, body) => {
-    try {
-      const { data } = await axios.post(`${API_PREFIX}${url}`, body);
-      if (!data.success) throw data;
-      return {
-        isSuccess: true,
-        data,
-      };
-    } catch (e) {
-      return {
-        isSuccess: false,
-        message: e.message,
-      };
-    }
-  },
-
-  update: async (url, body) => {
-    try {
-      const { data } = await axios.put(`${API_PREFIX}${url}`, body);
-      if (!data.success) throw new Error(data.message);
-      return {
-        isSuccess: true,
-        data,
-      };
-    } catch (e) {
-      return {
-        isSuccess: false,
-        message: e.message,
-      };
-    }
-  },
-
-  delete: async (url) => {
-    try {
-      const { data } = await axios.delete(`${API_PREFIX}${url}`);
-      if (!data.success) throw new Error(data.message);
-      return {
-        isSuccess: true,
-        data,
-      };
-    } catch (e) {
-      return {
-        isSuccess: false,
-        message: e.message,
-      };
-    }
-  },
+  get: async (url) => await request(url, 'get'),
+  post: async (url, body) => await request(url, 'post', body),
+  update: async (url, body) => await request(url, 'put', body),
+  delete: async (url) => await request(url, 'delete', body),
 };
