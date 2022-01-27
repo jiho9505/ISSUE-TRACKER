3abd94ae12b6d1db8d9c5333f94b5caa33c6841d
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-27 14:50:52 +0900

    feat : Add Board model

---
+++ server/models/Board.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+const model = mongoose.model;
+
+const boardSchema = new Schema(
+  {
+    writer: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: Array,
+      default: [],
+    },
+    isOpen: {
+      type: Boolean,
+      default: true,
+    },
+    closeTime: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+const Board = model('Board', boardSchema);
+
+export { Board };
