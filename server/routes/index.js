784a1d2f203c2497dae0013298b65a3d4f03ac91
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-27 11:14:13 +0900

    feat : Add Label Route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import UserRouter from './users.js';
+import LabelRouter from './label.js';
 
 const ApiRouter = Router();
 
 ApiRouter.use('/users', UserRouter);
+ApiRouter.use('/label', LabelRouter);
 
 export default ApiRouter;
---
+++ server/routes/label.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+import { LabelController } from './../controllers/LabelController.js';
+import { auth } from '../middleware/auth.js';
+
+const router = express.Router();
+
+router.get('/', auth.checkLogin, LabelController.getLabels);
+router.get('/length', auth.checkLogin, LabelController.getLength);
+router.post('/', auth.checkLogin, LabelController.createLabel);
+router.patch('/', auth.checkLogin, LabelController.updateLabel);
+router.delete('/', auth.checkLogin, LabelController.deleteLabel);
+
+export default router;
