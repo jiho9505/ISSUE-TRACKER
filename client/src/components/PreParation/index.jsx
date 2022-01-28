6103cf5fc6dc70bf6c2b7fd060e6c4cf6aa0c3a1
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-28 18:17:41 +0900

    feat : 준비중 컴포넌트 추가

---
+++ client/src/components/PreParation/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+import { allCenterAlign } from '@/static/style/mixin';
+import { PREPARE } from '@/static/constants/image-path';
+import { useTheme } from '@emotion/react';
+
+const PreParation = () => {
+  const theme = useTheme();
+  return (
+    <PreParationContainer>
+      <PreParationSpan theme={theme}>Coming soon...</PreParationSpan>
+      <img src={PREPARE} alt="준비 중" />
+    </PreParationContainer>
+  );
+};
+
+export default PreParation;
+
+const PreParationContainer = styled.div`
+  ${allCenterAlign}
+  flex-direction: column;
+  background-color: white;
+  padding: 30px 0;
+
+  img {
+    margin-top: 50px;
+  }
+`;
+
+const PreParationSpan = styled.span`
+  font-size: 2rem;
+  font-weight: bold;
+  color: ${(props) => props.theme.colors.text};
+  margin-top: 20px;
+`;
