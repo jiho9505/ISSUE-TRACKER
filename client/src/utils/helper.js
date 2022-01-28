cc50cf3fa505d5794d1d4edcd5f84267da3c32bc
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-28 18:17:18 +0900

    feat : Add 랜덤컬러추출 fn

--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -6,6 +6,6 @@ export const delay = (time) => {
 
 export const getRandomColor = () => {
   const hexCode = Math.floor(Math.random() * 16777215).toString(16);
-  const result = hexCode.padStart(6, '0');
-  return result;
+  const result = '#' + hexCode.padStart(6, '0');
+  return result.toLocaleUpperCase();
 };
