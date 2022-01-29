adcda2ac239a8c8663a4fa90cbd46b8b4da295b4
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-29 17:43:44 +0900

    feat : Add LabelButton Component

---
+++ client/src/components/LabelButton/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+function LabelButton({ info }) {
+  return (
+    <Button bgColor={info.bgColor} textColor={info.textColor}>
+      {info.name}
+    </Button>
+  );
+}
+
+export default LabelButton;
+
+const Button = styled.button`
+  border-radius: 30px;
+  background-color: ${(props) => props.bgColor};
+  color: ${(props) => props.textColor};
+  padding: 8px 12px;
+  font-size: 12px;
+  min-width: 120px;
+  height: 30px;
+`;
