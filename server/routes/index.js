75234b135abe3928e6c97f72f3f5ec2ad390177f
fleek-moon <fleek.moon@kakaocorp.com>
2022-01-30 09:27:14 +0900

    feat : Activate Label route

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,7 @@ import IssueFilterRouter from './issue-filter.js';
 const ApiRouter = Router();
 
 ApiRouter.use('/users', UserRouter);
-// ApiRouter.use('/label', LabelRouter);
+ApiRouter.use('/label', LabelRouter);
 ApiRouter.use('/issue-filter', IssueFilterRouter);
 
 export default ApiRouter;
