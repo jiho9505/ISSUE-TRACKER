be60586f0d81ac564b8dd93ee4576892cd223f25
fleek-moon <fleek.moon@kakaocorp.com>
2022-02-01 22:19:22 +0900

    feat : Add refresh token logic in user service

--- server/services/UserService.js
+++ server/services/UserService.js
@@ -1,36 +1,42 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+
 import { User } from '../models/User.js';
+
 import { error } from '../errors/index.js';
 
+const getFirstChar = async (_id) => {
+  const user = await User.findOne({ _id });
+  const userFirstChar = user.id[0];
+  return userFirstChar;
+};
+
 const login = async (id, password) => {
-  try {
-    const user = await User.findOne({ id });
-    if (!user) throw new Error(error.INCORRECT_ID_ERROR);
+  const user = await User.findOne({ id });
+  if (!user) throw new Error(error.INCORRECT_ID_ERROR);
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) throw new Error(error.INCORRECT_PWD_ERROR);
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) throw new Error(error.INCORRECT_PWD_ERROR);
 
-    const token = jwt.sign({ _id: user._id }, 'secret', { expiresIn: '1d' });
-    return token;
-  } catch (e) {
-    throw e;
-  }
+  const accessToken = jwt.sign({ _id: user._id }, 'secret', { expiresIn: '7000' });
+  const refreshToken = jwt.sign({ _id: user._id }, 'secret', { expiresIn: '14d' });
+
+  await User.findOneAndUpdate({ id }, { refreshToken });
+  return [accessToken, refreshToken];
 };
 
 const registerUser = async (data) => {
-  try {
-    const userInfo = await User.findOne({ id: data.id });
-    if (userInfo) throw new Error(error.CHECK_ID_ERROR);
-    const user = new User(data);
-    await user.save();
-  } catch (e) {
-    throw e;
-  }
+  const userInfo = await User.findOne({ id: data.id });
+  if (userInfo) throw new Error(error.CHECK_ID_ERROR);
+  const user = new User(data);
+  await user.save();
 };
 
-const logout = (res) => {
-  res.clearCookie('w_auth');
+const logout = async (req, res) => {
+  const _id = req._id;
+  res.clearCookie('accessToken');
+  res.clearCookie('refreshToken');
+  await User.findOneAndUpdate({ _id }, { refreshToken: '' });
 };
 
-export const UserService = { logout, registerUser, login };
+export const UserService = { logout, registerUser, login, getFirstChar };
