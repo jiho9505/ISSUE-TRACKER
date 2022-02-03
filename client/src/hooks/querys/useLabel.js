48a660bf3789803f41c2f71a4ff6cdc097d7a8c0
fleek-moon <fleek.moon@kakaocorp.com>
2022-02-03 17:18:46 +0900

    feat : useLabelQuery에 refetchInterval 적용

--- client/src/hooks/querys/useLabel.js
+++ client/src/hooks/querys/useLabel.js
@@ -1,10 +1,24 @@
 import { useQuery } from 'react-query';
 import { GET } from '@/api/base';
-import { processData } from './queryCommon';
+import { processQueryData } from './queryCommon';
+
+const LABEL_REFETCH = 1000 * 60 * 5;
 
 export const useLabelQuery = () => {
-  const { data } = useQuery('LABEL', () => GET('/label'));
+  const { data } = useQuery('LABEL', () => GET('/label'), {
+    refetchInterval: LABEL_REFETCH,
+  });
 
-  const result = processData(data);
+  const result = processQueryData(data);
+  return result;
+};
+
+export const useLabelLengthQuery = () => {
+  const { data } = useQuery(['LABEL', 'LENGTH'], () => GET('/label/length'), {
+    initialData: 0,
+    refetchInterval: LABEL_REFETCH,
+  });
+
+  const result = processQueryData(data);
   return result;
 };
