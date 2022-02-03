862d9787af3229a9ab9ddf0a0f0176e355e01075
fleek-moon <fleek.moon@kakaocorp.com>
2022-02-03 09:27:51 +0900

    refactor : Remove magic number

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -2,9 +2,13 @@ import jwt from 'jsonwebtoken';
 
 import { User } from '../models/User.js';
 
+const JWT_KEY = 'secret';
+const ACCESS_TOKEN_EXPIRE = '1h';
+const REFRESH_TOKEN_EXPIRE = '14d';
+
 export const createJWT = async (_id) => {
-  const accessToken = jwt.sign({ _id }, 'secret', { expiresIn: '1h' });
-  const refreshToken = jwt.sign({ _id }, 'secret', { expiresIn: '14d' });
+  const accessToken = jwt.sign({ _id }, JWT_KEY, { expiresIn: ACCESS_TOKEN_EXPIRE });
+  const refreshToken = jwt.sign({ _id }, JWT_KEY, { expiresIn: REFRESH_TOKEN_EXPIRE });
   await User.findOneAndUpdate({ _id }, { refreshToken });
   return [accessToken, refreshToken];
 };
