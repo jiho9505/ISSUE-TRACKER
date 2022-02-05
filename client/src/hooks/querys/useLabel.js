e39b19403de3935b8181b55718dc1d6eff76a16c
fleek-moon <fleek.moon@kakaocorp.com>
2022-02-05 14:32:59 +0900

    refactor : UseQuery isLoading 추가

--- client/src/hooks/querys/useLabel.js
+++ client/src/hooks/querys/useLabel.js
@@ -5,20 +5,18 @@ import { processQueryData } from './queryCommon';
 const LABEL_REFETCH = 1000 * 60 * 5;
 
 export const useLabelQuery = () => {
-  const { data } = useQuery('LABEL', () => GET('/label'), {
+  const { data, isLoading } = useQuery('LABEL', () => GET('/label'), {
     refetchInterval: LABEL_REFETCH,
   });
-
   const result = processQueryData(data);
-  return result;
+  return [result, isLoading];
 };
 
 export const useLabelLengthQuery = () => {
   const { data } = useQuery(['LABEL', 'LENGTH'], () => GET('/label/length'), {
-    initialData: 0,
     refetchInterval: LABEL_REFETCH,
   });
 
   const result = processQueryData(data);
-  return result;
+  return result ? result : 0;
 };
