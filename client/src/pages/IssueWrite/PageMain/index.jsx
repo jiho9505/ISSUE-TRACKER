bc38a9634cbc41b45a41eeb94441facff36af86e
fleek-moon <fleek.moon@kakaocorp.com>
2022-02-05 15:13:47 +0900

    feat : 이슈 작성 Form 이벤트 추가

--- client/src/pages/IssueWrite/PageMain/index.jsx
+++ client/src/pages/IssueWrite/PageMain/index.jsx
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 import { useTheme } from '@emotion/react';
+import { useSetRecoilState } from 'recoil';
 
 import ProfileImage from '@/components/ProfileImage';
 import WritingPart from '../WritingPart';
 import IssueOptionContainer from '@/components/IssueOptionContainer';
 import GreyLine from '@/components/GreyLine';
 import CustomButton from '@/components/CustomButton';
+import { useNavigate } from '@/core/Router';
 
 import { useUserImageQuery } from '@/hooks/querys/useUser';
+import { api } from '@/api/base';
+import { toastAtom } from '@/store/toastState';
+
+let timer = 0;
+const switchPageTime = 2000;
 
 function PageMain() {
   const theme = useTheme();
+  const setToast = useSetRecoilState(toastAtom);
+  const navigateTo = useNavigate();
   const [title, setTitle] = useState('');
   const [comment, setComment] = useState({
     content: '',
@@ -29,6 +38,10 @@ function PageMain() {
     }
     setisFilledForm(false);
   }, [title, comment]);
+
+  useEffect(() => {
+    return () => clearTimeout(timer);
+  }, []);
 
   const refreshState = (key, newState) => {
     switch (key) {
@@ -49,8 +62,43 @@ function PageMain() {
     }
   };
 
+  const handleSubmitForm = async (e) => {
+    e.preventDefault();
+    const body = {
+      title,
+      comment,
+      assignees,
+      labels,
+    };
+
+    const result = await api.post('/board', body);
+
+    if (result.isSuccess) {
+      setToast({
+        isActive: true,
+        title: '이슈 작성이 되었습니다❗️',
+        content: '2초 후 메인페이지로 이동합니다',
+        mode: 'success',
+      });
+      switchPage();
+    }
+    if (!result.isSuccess) {
+      setToast({
+        isActive: true,
+        title: result.message,
+        mode: 'fail',
+      });
+    }
+  };
+
+  const switchPage = () => {
+    timer = setTimeout(() => {
+      navigateTo('/');
+    }, switchPageTime);
+  };
+
   return (
-    <PageMainContainer>
+    <Form onSubmit={handleSubmitForm}>
       <ContentContainer>
         <ProfileImage imageSrc={imageSrc} />
         <WritingPart refreshState={refreshState} />
@@ -63,17 +111,18 @@ function PageMain() {
           opacity={isFilledForm ? '1' : '0.5'}
           sizeLevel={2}
           bgColor={theme.colors.blue}
+          onSubmit={handleSubmitForm}
         >
           완료
         </CustomButton>
       </ButtonContainer>
-    </PageMainContainer>
+    </Form>
   );
 }
 
 export default PageMain;
 
-const PageMainContainer = styled.div``;
+const Form = styled.form``;
 
 const ContentContainer = styled.div`
   display: flex;
