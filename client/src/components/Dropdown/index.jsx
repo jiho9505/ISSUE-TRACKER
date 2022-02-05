fca7cbed7a795978184f9d5dbfccf8f164fa5d63
fleek-moon <fleek.moon@kakaocorp.com>
2022-02-05 16:21:36 +0900

    refactor : 재사용성 높이는 방향으로 구현

--- client/src/components/Dropdown/index.jsx
+++ client/src/components/Dropdown/index.jsx
@@ -2,31 +2,15 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { useTheme } from '@emotion/react';
 
-import { CHECKED_CIRCLE, CIRCLE } from '@/static/constants/image-path';
 import { seroCenterAlign } from '@/static/style/mixin';
 
-const titlesHavingCircle = ['이슈 필터'];
-
-function Dropdown({ className, title, items, onMouseLeave, onClick, choicedIdx }) {
+function Dropdown({ className, children, title, onMouseLeave }) {
   const theme = useTheme();
-
-  const createCircleImg = (idx) => {
-    if (titlesHavingCircle.includes(title)) {
-      return idx === choicedIdx ? <img src={CHECKED_CIRCLE} /> : <img src={CIRCLE} />;
-    }
-  };
-
-  const createItems = items?.map((item, idx) => (
-    <Item key={item._id} theme={theme.colors} onClick={onClick} data-idx={idx}>
-      <span>{item.name}</span>
-      {createCircleImg(idx)}
-    </Item>
-  ));
 
   return (
     <DropdownContainer onMouseLeave={onMouseLeave} className={className} theme={theme.colors}>
       {title && <Title>{title}</Title>}
-      {createItems}
+      {children}
     </DropdownContainer>
   );
 }
@@ -49,15 +33,4 @@ const Title = styled.div`
   ${seroCenterAlign}
   padding: 8px 16px;
   height: 48px;
-`;
-
-const Item = styled.div`
-  ${seroCenterAlign}
-  justify-content: space-between;
-  background-color: white;
-  padding: 8px 16px;
-  color: ${(props) => props.theme.text};
-  height: 48px;
-  border-top: 1px solid ${(props) => props.theme.border};
-  cursor: pointer;
 `;
