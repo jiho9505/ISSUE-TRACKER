d722c98c263fccd002a438b26beeaa18868de145
fleek-moon <fleek.moon@kakaocorp.com>
2022-02-06 17:02:08 +0900

    feat : Add useIssueQuery

--- client/src/hooks/querys/useIssue.js
+++ client/src/hooks/querys/useIssue.js
@@ -4,6 +4,22 @@ import { processQueryData } from './queryCommon';
 
 const ISSUE_REFETCH = 1000 * 60 * 5;
 
+export const useIssueQuery = (paramGetIssue) => {
+  const status = paramGetIssue.status;
+  const filter = paramGetIssue.filter;
+  const page = paramGetIssue.page;
+
+  const { data, isLoading } = useQuery(
+    'ISSUE',
+    () => GET(`/issue?status=${status}&filter=${filter}&page=${page}`),
+    {
+      refetchInterval: ISSUE_REFETCH,
+    }
+  );
+  const result = processQueryData(data);
+  return [result, isLoading];
+};
+
 export const useIssueLengthQuery = (paramGetIssue) => {
   const filter = paramGetIssue.filter;
   const { data } = useQuery(['ISSUE', 'LENGTH'], () => GET(`/issue/length?filter=${filter}`), {
