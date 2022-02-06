13fe81d13e156bdc388a2e8a980c7e195a5a40cb
fleek-moon <fleek.moon@kakaocorp.com>
2022-02-06 18:08:32 +0900

    feat : Add ContentMain Component

---
+++ client/src/pages/Main/ContentMain/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { useRecoilValue } from 'recoil';
+
+import Issue from '../Issue';
+import Empty from '@/components/Empty';
+import Loader from '@/components/Loader';
+
+import { useIssueQuery } from '@/hooks/querys/useIssue';
+import { paramGetIssueAtom } from '@/store/getIssueParamState';
+
+function ContentMain() {
+  const paramGetIssue = useRecoilValue(paramGetIssueAtom);
+  const [Issues, isLoading] = useIssueQuery(paramGetIssue);
+
+  if (isLoading) return <Loader />;
+  if (Issues?.length === 0) return <Empty />;
+
+  return (
+    <ContentMainContainer>
+      {Issues.map((info) => (
+        <Issue key={info.issue._id} info={info} />
+      ))}
+    </ContentMainContainer>
+  );
+}
+
+export default ContentMain;
+
+const ContentMainContainer = styled.div``;
