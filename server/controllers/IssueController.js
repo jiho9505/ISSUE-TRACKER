06f145f1f8963dc03e4f46981de5175d8deb9f75
fleek-moon <fleek.moon@kakaocorp.com>
2022-02-06 13:18:12 +0900

    feat : Add getIssues api

--- server/controllers/IssueController.js
+++ server/controllers/IssueController.js
@@ -1,6 +1,20 @@
 import { IssueService } from '../services/IssueService.js';
 import { error } from '../errors/index.js';
 
+const getIssues = async (req, res) => {
+  try {
+    const status = req.query.status;
+    const filter = req.query.filter;
+    const page = Number(req.query.page) - 1;
+    const writer = req._id;
+    const result = await IssueService.getIssues(status, filter, writer, page);
+    return res.json({ success: true, result });
+  } catch (e) {
+    console.log(e);
+    return res.json({ success: false, message: error.GET_ISSUE_ERROR });
+  }
+};
+
 const createIssue = async (req, res) => {
   try {
     const body = req.body;
@@ -13,11 +27,64 @@ const createIssue = async (req, res) => {
   }
 };
 
+// const getDetail = async (req, res) => {
+//   try {
+//     const issueId = req.query.id;
+//     const result = await IssueService.getDetail(issueId);
+
+//     return res.json({ success: true, result });
+//   } catch (e) {
+//     return res.json({ success: false, message: error.GET_BOARD_ERROR });
+//   }
+// };
+
+// const createImage = async (req, res) => {
+//   const upload = await IssueService.createImage();
+//   upload(req, res, (err) => {
+//     if (err) {
+//       return res.json({ success: false, message: error.CREATE_IMAGE_ERROR });
+//     }
+//     const file = res.req.file;
+//     return res.json({ success: true, filePath: file.location });
+//   });
+// };
+
+// const updateIssue = async (req, res) => {
+//   try {
+//     const issueId = req.query.id;
+//     const body = {
+//       _id: issueId,
+//     };
+//     const { title, description, images, chooseBoard } = req.body;
+//     const content = {
+//       title,
+//       description,
+//       images,
+//       chooseBoard,
+//     };
+
+//     await IssueService.updateIssue(body, content);
+//     return res.json({ success: true });
+//   } catch (e) {
+//     return res.json({ success: false, message: error.UPDATE_BOARD_ERROR });
+//   }
+// };
+
+// const deleteIssue = async (req, res) => {
+//   try {
+//     const issueId = req.query.id;
+//     await IssueService.deleteIssue(issueId);
+//     return res.json({ success: true });
+//   } catch (e) {
+//     return res.json({ success: false, message: error.DELETE_BOARD_ERROR });
+//   }
+// };
+
 export const IssueController = {
-  // getIssues,
+  getIssues,
+  createIssue,
   // getDetail,
   // createImage,
-  createIssue,
   // updateIssue,
   // deleteIssue,
 };
--- server/services/IssueService.js
+++ server/services/IssueService.js
@@ -7,6 +7,115 @@ import { Comment } from '../models/Comment.js';
 import { Assignee } from '../models/Assignee.js';
 import { IssueLabel } from '../models/IssueLabel.js';
 
+/**
+ * @filter
+ * 0: 내가 작성한 이슈
+ * 1: 나에게 할당된 이슈
+ * 2: 내가 댓글을 남긴 이슈
+ */
+const getIssues = async (status, filter, writer, page) => {
+  const LIMIT = 5;
+  const isOpenBody = status === 'open' ? { isOpen: true } : { isOpen: false };
+  const result = [];
+
+  const pushCoreContent = (issue, assignees, issueLabels) => {
+    const issueResult = {
+      _id: issue._id,
+      title: issue.title,
+      isOpen: issue.isOpen,
+      writerName: issue.writer.name,
+    };
+    const assigneeResult = assignees.map((assignee) => ({
+      avatar: assignee.userId.avatar,
+    }));
+    const issueLabelResult = issueLabels.map((issueLabel) => ({
+      name: issueLabel.labelId.name,
+      bgColor: issueLabel.labelId.bgColor,
+      textColor: issueLabel.labelId.textColor,
+    }));
+
+    result.push({
+      issue: issueResult,
+      assignees: assigneeResult,
+      labels: issueLabelResult,
+    });
+  };
+
+  const queryExec = async (issueId, mode = '') => {
+    if (mode === 'all') {
+      const [issue, assignees, issueLabels] = await Promise.all([
+        Issue.findOne({ _id: issueId, ...isOpenBody }).populate('writer'),
+        Assignee.find({ issueId }).populate('userId'),
+        IssueLabel.find({ issueId }).populate('labelId'),
+      ]);
+      return [issue, assignees, issueLabels];
+    }
+    const [assignees, issueLabels] = await Promise.all([
+      Assignee.find({ issueId }).populate('userId'),
+      IssueLabel.find({ issueId }).populate('labelId'),
+    ]);
+    return [assignees, issueLabels];
+  };
+
+  switch (filter) {
+    case '0':
+      const issuesAuthorMe = await Issue.find({ writer, ...isOpenBody })
+        .populate('writer')
+        .sort({ createdAt: -1 })
+        .skip(page * LIMIT)
+        .limit(LIMIT);
+
+      await Promise.all(
+        issuesAuthorMe.map(async (issue) => {
+          const [assignees, issueLabels] = await queryExec(issue._id);
+          pushCoreContent(issue, assignees, issueLabels);
+        })
+      );
+      break;
+    case '1':
+      const assigneeArr = await Assignee.find({ userId: writer })
+        .sort({ createdAt: -1 })
+        .skip(page * LIMIT)
+        .limit(LIMIT);
+
+      await Promise.all(
+        assigneeArr.map(async (assignee) => {
+          const issueId = assignee.issueId;
+          const [issue, assignees, issueLabels] = await queryExec(issueId, 'all');
+          pushCoreContent(issue, assignees, issueLabels);
+        })
+      );
+      break;
+    case '2':
+      const issueIdArr = await Comment.find({ writer }).distinct('issueId');
+      const organizedIssueIdArr = issueIdArr.reverse().slice(page * LIMIT, LIMIT);
+
+      await Promise.all(
+        organizedIssueIdArr.map(async (issueId) => {
+          const [issue, assignees, issueLabels] = await queryExec(issueId, 'all');
+          pushCoreContent(issue, assignees, issueLabels);
+        })
+      );
+      break;
+    default:
+      const issues = await Issue.find(isOpenBody)
+        .populate('writer')
+        .sort({ createdAt: -1 })
+        .skip(page * LIMIT)
+        .limit(LIMIT);
+
+      await Promise.all(
+        issues.map(async (issue) => {
+          const [assignees, issueLabels] = await queryExec(issue._id);
+          pushCoreContent(issue, assignees, issueLabels);
+        })
+      );
+      break;
+  }
+
+  return result;
+};
+
 const createIssue = async (body, writer) => {
   const arr = ['-'];
   const issueBody = { writer, title: body.title };
@@ -32,11 +141,64 @@ const createIssue = async (body, writer) => {
   await Promise.all([...assigneePromise, ...issueLabelPromise, ...commentPromise, issue.save()]);
 };
 
+// const getDetail = async (issueId) => {
+//   const [issue, comments, assignees, issueLabels] = await Promise.all([
+//     Issue.findOne({ _id: issueId }).populate('writer'),
+//     Comment.find({ issueId }).populate('writer'),
+//     Assignee.find({ issueId }).populate('userId'),
+//     IssueLabel.find({ issueId }).populate('labelId'),
+//   ]);
+//   console.log(issue, comments, assignees, issueLabels);
+//   return result;
+// };
+
+// const createImage = async () => {
+//   const s3 = new AWS.S3({
+//     accessKeyId: config.accessKeyId,
+//     secretAccessKey: config.secretAccessKey,
+//     region: config.region,
+//   });
+
+//   const storage = multerS3({
+//     s3: s3,
+//     bucket: 'fgd-storeimage',
+//     contentType: multerS3.AUTO_CONTENT_TYPE,
+//     acl: 'public-read',
+//     metadata: function (req, file, cb) {
+//       cb(null, { fieldName: file.fieldname });
+//     },
+//     key: function (req, file, cb) {
+//       cb(null, `storeImages/${Date.now()}_${file.originalname}`);
+//     },
+//   });
+
+//   const upload = multer({ storage }).single('file');
+//   return upload;
+// };
+
+// const updateIssue = async (body, content) => {
+//   try {
+//     await Board.findOneAndUpdate(body, content);
+//     return;
+//   } catch (e) {
+//     throw e;
+//   }
+// };
+
+// const deleteIssue = async (boardId) => {
+//   try {
+//     await Promise.all([Board.findOneAndDelete({ _id: boardId }), Comment.deleteMany({ boardId })]);
+//     return;
+//   } catch (e) {
+//     throw e;
+//   }
+// };
+
 export const IssueService = {
-  // getIssues,
+  getIssues,
+  createIssue,
   // getDetail,
   // createImage,
-  createIssue,
   // updateIssue,
   // deleteIssue,
 };
