811eb34f7d7b86d2551fa7463d9b95435d6c8527
fleek-moon <fleek.moon@kakaocorp.com>
2022-02-07 17:12:43 +0900

    feat : Add IssueToggleButton Component

---
+++ client/src/pages/IssueDetail/IssueToggleButton/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { useTheme } from '@emotion/react';
+
+import CustomButton from '@/components/CustomButton';
+
+import { BOX_BLUE } from '@/static/constants/image-path';
+
+function IssueToggleButton() {
+  const theme = useTheme();
+  return (
+    <ToggleButton sizeLevel={1} theme={theme}>
+      <img src={BOX_BLUE} />
+      <span>이슈 닫기</span>
+    </ToggleButton>
+  );
+}
+
+export default IssueToggleButton;
+
+const ToggleButton = styled(CustomButton)`
+  background-color: white;
+  border: 1px solid ${(props) => props.theme.colors.blue};
+  span {
+    color: ${(props) => props.theme.colors.blue};
+  }
+`;
