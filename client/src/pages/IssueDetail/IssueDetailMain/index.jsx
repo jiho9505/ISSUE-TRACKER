f4f2e2d16d7076d302f446b2b14564cef7cbb80c
fleek-moon <fleek.moon@kakaocorp.com>
2022-02-08 12:40:45 +0900

    feat : 이슈 삭제 기능 추가

--- client/src/pages/IssueDetail/IssueDetailMain/index.jsx
+++ client/src/pages/IssueDetail/IssueDetailMain/index.jsx
@@ -10,25 +10,33 @@ import IssueOptionContainer from '@/components/IssueOptionContainer';
 import Comment from '../Comment';
 import Loader from '@/components/Loader';
 import CustomButton from '@/components/CustomButton';
+import Alert from '@/components/Alert';
 
 import { useUserImageQuery } from '@/hooks/querys/useUser';
 import { useCommentQuery } from '@/hooks/querys/useComment';
 import { getRouterParams } from '@/utils/helper';
-import { PLUS_WHITE } from '@/static/constants/image-path';
-import { POST } from '@/api/base';
+import { PLUS_WHITE, TRASH } from '@/static/constants/image-path';
+import { POST, DELETE } from '@/api/base';
 import { toastAtom } from '@/store/toastState';
+import { seroCenterAlign } from '@/static/style/mixin';
+import { useNavigate } from '@/core/Router';
 
+let timer = 0;
 const disableOpacity = 0.5;
 const enableOpacity = 1;
+const switchPageTime = 2000;
 
 function IssueDetailMain() {
+  const navigateTo = useNavigate();
   const theme = useTheme();
   const issueId = getRouterParams();
   const [comments, isLoading] = useCommentQuery(issueId);
   const userImage = useUserImageQuery();
   const [commentText, setCommentText] = useState('');
+  const [isDeleteMode, setIsDeleteMode] = useState(false);
   const [opacity, setOpacity] = useState(disableOpacity);
   const postMutation = useMutation((body) => POST('/comment', body));
+  const deleteMutation = useMutation(() => DELETE(`/issue?id=${issueId}`));
   const queryClient = useQueryClient();
   const setToast = useSetRecoilState(toastAtom);
 
@@ -36,6 +44,11 @@ function IssueDetailMain() {
     commentText ? setOpacity(enableOpacity) : setOpacity(disableOpacity);
   }, [commentText]);
 
+  useEffect(() => {
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleClickDeleteIssueButton = () => setIsDeleteMode(true);
   const refreshState = (_, { content }) => setCommentText(content);
   const handleClickCommentButtonBtn = async () => {
     const body = { issueId, content: commentText };
@@ -59,7 +72,36 @@ function IssueDetailMain() {
     queryClient.invalidateQueries('COMMENT');
     setCommentText('');
   };
+  const handleClickAlert = async (e) => {
+    if (['alert__overlay', 'alert__cancel'].includes(e.target.className)) setIsDeleteMode(false);
+    if (e.target.className === 'alert__delete') {
+      setIsDeleteMode(false);
+      const result = await deleteMutation.mutateAsync();
 
+      if (!result.data.success) {
+        setToast({
+          isActive: true,
+          title: result.data.message,
+          mode: 'fail',
+        });
+        return;
+      }
+
+      setToast({
+        isActive: true,
+        title: '이슈를 삭제하였습니다.',
+        content: '2초 후 메인페이지로 이동합니다',
+        mode: 'success',
+      });
+      switchPage();
+    }
+  };
+
+  const switchPage = () => {
+    timer = setTimeout(() => {
+      navigateTo('/main');
+    }, switchPageTime);
+  };
   if (isLoading) return <Loader />;
 
   return (
@@ -85,8 +127,14 @@ function IssueDetailMain() {
           </CommentButton>
         </CommentButtonContainer>
       </CommentContainer>
-
-      <IssueOptionContainer />
+      <RightContainer>
+        <IssueOptionContainer />
+        <DeleteIssueButton onClick={handleClickDeleteIssueButton}>
+          <img src={TRASH} />
+          <span>이슈 삭제</span>
+        </DeleteIssueButton>
+      </RightContainer>
+      {isDeleteMode && <Alert onClick={handleClickAlert} />}
     </IssueDetailMainContainer>
   );
 }
@@ -116,4 +164,20 @@ const CommentButtonContainer = styled.div`
 
 const CommentButton = styled(CustomButton)`
   width: 130px;
+`;
+
+const RightContainer = styled.div`
+  height: 100%;
+`;
+
+const DeleteIssueButton = styled.div`
+  ${seroCenterAlign}
+  gap: 5px;
+  width: 90%;
+  margin-top: 20px;
+  justify-content: flex-end;
+  cursor: pointer;
+  span {
+    color: ${({ theme }) => theme.colors.red};
+  }
 `;
