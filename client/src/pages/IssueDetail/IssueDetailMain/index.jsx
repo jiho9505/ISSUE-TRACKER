7b2101ee579820ec1b017897f0401aac953553a2
fleek-moon <fleek.moon@kakaocorp.com>
2022-02-08 12:18:17 +0900

    feat : 코멘트 생성 서버와 연결

--- client/src/pages/IssueDetail/IssueDetailMain/index.jsx
+++ client/src/pages/IssueDetail/IssueDetailMain/index.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 import { useTheme } from '@emotion/react';
+import { useMutation, useQueryClient } from 'react-query';
+import { useSetRecoilState } from 'recoil';
 
 import ProfileImage from '@/components/ProfileImage';
 import WritingComment from '@/components/WritingComment';
@@ -13,6 +15,8 @@ import { useUserImageQuery } from '@/hooks/querys/useUser';
 import { useCommentQuery } from '@/hooks/querys/useComment';
 import { getRouterParams } from '@/utils/helper';
 import { PLUS_WHITE } from '@/static/constants/image-path';
+import { POST } from '@/api/base';
+import { toastAtom } from '@/store/toastState';
 
 const disableOpacity = 0.5;
 const enableOpacity = 1;
@@ -24,11 +28,37 @@ function IssueDetailMain() {
   const userImage = useUserImageQuery();
   const [commentText, setCommentText] = useState('');
   const [opacity, setOpacity] = useState(disableOpacity);
-  const refreshState = (_, { content }) => setCommentText(content);
+  const postMutation = useMutation((body) => POST('/comment', body));
+  const queryClient = useQueryClient();
+  const setToast = useSetRecoilState(toastAtom);
 
   useEffect(() => {
     commentText ? setOpacity(enableOpacity) : setOpacity(disableOpacity);
   }, [commentText]);
+
+  const refreshState = (_, { content }) => setCommentText(content);
+  const handleClickCommentButtonBtn = async () => {
+    const body = { issueId, content: commentText };
+    const result = await postMutation.mutateAsync(body);
+
+    if (!result.data.success) {
+      setToast({
+        isActive: true,
+        title: result.data.message,
+        mode: 'fail',
+      });
+      return;
+    }
+
+    setToast({
+      isActive: true,
+      title: '코멘트를 생성하였습니다.',
+      mode: 'success',
+    });
+
+    queryClient.invalidateQueries('COMMENT');
+    setCommentText('');
+  };
 
   if (isLoading) return <Loader />;
 
@@ -41,11 +71,11 @@ function IssueDetailMain() {
 
         <WritingPart>
           <ProfileImage imageSrc={userImage} />
-          <WritingComment refreshState={refreshState} />
+          <WritingComment refreshState={refreshState} value={commentText} />
         </WritingPart>
         <CommentButtonContainer>
           <CommentButton
-            // onClick={handleClickWritingIssueBtn}
+            onClick={handleClickCommentButtonBtn}
             sizeLevel={1}
             bgColor={theme.colors.blue}
             opacity={opacity}
@@ -56,7 +86,7 @@ function IssueDetailMain() {
         </CommentButtonContainer>
       </CommentContainer>
 
-      <IssueOptionContainer refreshState={refreshState} />
+      <IssueOptionContainer />
     </IssueDetailMainContainer>
   );
 }
